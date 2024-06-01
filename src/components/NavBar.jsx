176db5ab990c40e07d33cb0f9167342569c83592
Samir Ahmed <samirakarez@gmail.com>
2024-06-01 19:16:18 -0400

    add mobile menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,63 +1,105 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
+import { FaAlignRight, FaAnglesRight } from 'react-icons/fa6';
 
 function NavBar() {
-    const [show, setShow] = useState(true);
-    let lastScrollY = window.scrollY;
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-    const controlNavbar = () => {
-        if (window.scrollY > lastScrollY) {
-            setShow(false);
-        } else {
-            setShow(true);
-        }
-        lastScrollY = window.scrollY;
+    const navBarHeight = 70;
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
     };
 
-    useEffect(() => {
-        window.addEventListener('scroll', controlNavbar);
-        return () => {
-            window.removeEventListener('scroll', controlNavbar);
-        };
-    }, []);
-
     return (
-        <div className={`fixed w-full h-28 bg-gray-100 p-12 flex justify-between items-center transition-transform duration-500 ${show ? 'transform translate-y-0' : 'transform -translate-y-full'} z-50`}>
-            <div className='logo'>
-                <Link to='home' smooth={true} duration={500} className='h-10 text-5xl font-mono font-regular cursor-pointer'>
-                    S/A
-                </Link>
+        <>
+            <div className={`fixed w-full md:h-28 h-20 bg-gray-100 md:p-12 p-4 flex justify-between items-center z-50 transition-all duration-300 ${isMobileMenuOpen ? 'backdrop-blur' : ''}`}>
+                <div className="relative z-50">
+                    <Link to='home' smooth={true} duration={500} hashSpy={true} className='h-10 md:text-5xl text-4xl font-mono font-regular cursor-pointer'>
+                        S/A
+                    </Link>
+                </div>
+                <div className='block md:hidden relative z-50'>
+                    <button onClick={toggleMobileMenu} className="text-3xl">
+                        <FaAlignRight />
+                    </button>
+                </div>
+                <ul className='hidden items-center md:flex relative z-40'>
+                    <li className='ml-8'>
+                        <Link to='about' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono relative cursor-pointer before:content-[""] 
+                        before:absolute before:w-[130%] before:h-1 before:bg-black before:bottom-[-8px] before:left-[-15%] before:origin-left 
+                        before:scale-x-0 before:transition-transform before:duration-300 hover:before:scale-x-100'>
+                            //ABOUT
+                        </Link>
+                    </li>
+                    <li className='ml-8'>
+                        <Link to='research' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono relative cursor-pointer before:content-[""] 
+                        before:absolute before:w-[130%] before:h-1 before:bg-black before:bottom-[-8px] before:left-[-15%] before:origin-left 
+                        before:scale-x-0 before:transition-transform before:duration-300 hover:before:scale-x-100'>
+                            //RESEARCH
+                        </Link>
+                    </li>
+                    <li className='ml-8'>
+                        <Link to='experience' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono relative cursor-pointer before:content-[""] 
+                        before:absolute before:w-[130%] before:h-1 before:bg-black before:bottom-[-8px] before:left-[-15%] before:origin-left 
+                        before:scale-x-0 before:transition-transform before:duration-300 hover:before:scale-x-100'>
+                            //EXPERIENCE
+                        </Link>
+                    </li>
+                    <li className='ml-8'>
+                        <Link to='projects' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono relative cursor-pointer before:content-[""] 
+                        before:absolute before:w-[130%] before:h-1 before:bg-black before:bottom-[-8px] before:left-[-15%] before:origin-left 
+                        before:scale-x-0 before:transition-transform before:duration-300 hover:before:scale-x-100'>
+                            //PROJECTS
+                        </Link>
+                    </li>
+                    <li className='ml-8'>
+                        <a href='mailto:ahmed1@usf.edu' target='_blank' rel='noopener noreferrer'>
+                            <button className='font-mono relative cursor-pointer before:content-[""] 
+                            before:absolute before:w-[130%] before:h-1 before:bg-black before:bottom-[-8px] before:left-[-15%] before:origin-left 
+                            before:scale-x-0 before:transition-transform before:duration-300 hover:before:scale-x-100'>
+                                <span className='relative z-10'>//CONTACT</span>
+                            </button>
+                        </a>
+                    </li>
+                </ul>
             </div>
-            <ul className='flex items-center'>
-                <li className='ml-8'>
-                    <Link to='about' smooth={true} duration={500} className='font-mono relative cursor-pointer before:content-[""] before:absolute before:w-[130%] before:h-1.5 before:bg-black before:bottom-[-8px] before:left-[-15%] before:origin-left before:scale-x-0 before:transition-transform before:duration-300 hover:before:scale-x-100'>
-                        ABOUT
-                    </Link>
-                </li>
-                <li className='ml-8'>
-                    <Link to='research' smooth={true} duration={500} className='font-mono relative cursor-pointer before:content-[""] before:absolute before:w-[130%] before:h-1.5 before:bg-black before:bottom-[-8px] before:left-[-15%] before:origin-left before:scale-x-0 before:transition-transform before:duration-300 hover:before:scale-x-100'>
-                        RESEARCH
-                    </Link>
-                </li>
-                <li className='ml-8'>
-                    <Link to='teaching' smooth={true} duration={500} className='font-mono relative cursor-pointer before:content-[""] before:absolute before:w-[130%] before:h-1.5 before:bg-black before:bottom-[-8px] before:left-[-15%] before:origin-left before:scale-x-0 before:transition-transform before:duration-300 hover:before:scale-x-100'>
-                        TEACHING
-                    </Link>
-                </li>
-                <li className='ml-8'>
-                    <Link to='projects' smooth={true} duration={500} className='font-mono relative cursor-pointer before:content-[""] before:absolute before:w-[130%] before:h-1.5 before:bg-black before:bottom-[-8px] before:left-[-15%] before:origin-left before:scale-x-0 before:transition-transform before:duration-300 hover:before:scale-x-100'>
-                        PROJECTS
-                    </Link>
-                </li>
-                <li className='ml-8'>
-                    <a href='mailto:ahmed1@usf.edu' target='_blank' rel='noopener noreferrer'>
-                        <button className='relative bg-transparent text-black font-mono font-semibold py-0.5 px-2 rounded border-black border-2 overflow-hidden before:content-[""] before:absolute before:inset-0 before:w-full before:h-full before:bg-black before:transform before:scale-x-0 before:origin-left before:transition-transform before:duration-300 hover:text-white hover:before:scale-x-100'>
-                            <span className='relative z-10'>CONTACT</span>
-                        </button>
-                    </a>
-                </li>
-            </ul>
-        </div>
+            {/* overlay for blur effect */}
+            <div className={`fixed inset-0 ${isMobileMenuOpen ? 'backdrop-blur-sm' : ''} z-40 pointer-events-none`}></div>
+            {/* mobile menu */}
+            <div className={`fixed top-0 right-0 h-full w-3/4 bg-gray-100 p-8 transition-transform duration-500 transform ${isMobileMenuOpen ? 'translate-x-0' : 'translate-x-full'} md:hidden z-50`}>
+                <button onClick={toggleMobileMenu} className="text-3xl absolute top-6 right-4">
+                    <FaAnglesRight />
+                </button>
+                <ul className='flex flex-col items-start mt-8'>
+                    <li className='mb-4'>
+                        <Link to='about' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono text-xl' onClick={toggleMobileMenu}>
+                            // ABOUT
+                        </Link>
+                    </li>
+                    <li className='mb-4'>
+                        <Link to='research' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono text-xl' onClick={toggleMobileMenu}>
+                            // RESEARCH
+                        </Link>
+                    </li>
+                    <li className='mb-4'>
+                        <Link to='experience' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono text-xl' onClick={toggleMobileMenu}>
+                            // EXPERIENCE
+                        </Link>
+                    </li>
+                    <li className='mb-4'>
+                        <Link to='projects' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono text-xl' onClick={toggleMobileMenu}>
+                            // PROJECTS
+                        </Link>
+                    </li>
+                    <li className='mb-4'>
+                        <a href='mailto:ahmed1@usf.edu' target='_blank' rel='noopener noreferrer' className='font-mono text-xl' onClick={toggleMobileMenu}>
+                            // CONTACT
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </>
     );
 }
 
