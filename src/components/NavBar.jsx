c7b51299e0ed369f3cb013a8c5b2aeaabd079ba8
Samir Ahmed <samirakarez@gmail.com>
2024-06-03 00:08:35 -0400

    update mobile menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-scroll';
-import { FaAlignRight, FaAnglesRight } from 'react-icons/fa6';
+import { RiMenu3Fill, RiCloseLine } from "react-icons/ri";
 
 function NavBar() {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -15,39 +15,39 @@ function NavBar() {
         <>
             <div className={`fixed w-full md:h-28 h-20 bg-gray-100 md:p-12 p-4 flex justify-between items-center z-50 transition-all duration-300 ${isMobileMenuOpen ? 'backdrop-blur' : ''}`}>
                 <div className="relative z-50">
-                    <Link to='home' smooth={true} duration={500} hashSpy={true} className='h-10 md:text-5xl text-4xl font-mono font-regular cursor-pointer'>
+                    <Link to='home' smooth={true} duration={500} hashSpy={true} className='h-12 md:h-10 md:text-5xl text-4xl font-mono cursor-pointer'>
                         S/A
                     </Link>
                 </div>
                 <div className='block md:hidden relative z-50'>
                     <button onClick={toggleMobileMenu} className="text-3xl">
-                        <FaAlignRight />
+                        <RiMenu3Fill />
                     </button>
                 </div>
                 <ul className='hidden items-center md:flex relative z-40'>
                     <li className='ml-8'>
-                        <Link to='about' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono relative cursor-pointer before:content-[""] 
+                        <Link to='about' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono font-medium relative cursor-pointer before:content-[""] 
                         before:absolute before:w-[130%] before:h-1 before:bg-black before:bottom-[-8px] before:left-[-15%] before:origin-left 
                         before:scale-x-0 before:transition-transform before:duration-300 hover:before:scale-x-100'>
                             //ABOUT
                         </Link>
                     </li>
                     <li className='ml-8'>
-                        <Link to='research' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono relative cursor-pointer before:content-[""] 
+                        <Link to='research' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono font-medium relative cursor-pointer before:content-[""] 
                         before:absolute before:w-[130%] before:h-1 before:bg-black before:bottom-[-8px] before:left-[-15%] before:origin-left 
                         before:scale-x-0 before:transition-transform before:duration-300 hover:before:scale-x-100'>
                             //RESEARCH
                         </Link>
                     </li>
                     <li className='ml-8'>
-                        <Link to='experience' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono relative cursor-pointer before:content-[""] 
+                        <Link to='experience' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono font-medium relative cursor-pointer before:content-[""] 
                         before:absolute before:w-[130%] before:h-1 before:bg-black before:bottom-[-8px] before:left-[-15%] before:origin-left 
                         before:scale-x-0 before:transition-transform before:duration-300 hover:before:scale-x-100'>
                             //EXPERIENCE
                         </Link>
                     </li>
                     <li className='ml-8'>
-                        <Link to='projects' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono relative cursor-pointer before:content-[""] 
+                        <Link to='projects' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono font-medium relative cursor-pointer before:content-[""] 
                         before:absolute before:w-[130%] before:h-1 before:bg-black before:bottom-[-8px] before:left-[-15%] before:origin-left 
                         before:scale-x-0 before:transition-transform before:duration-300 hover:before:scale-x-100'>
                             //PROJECTS
@@ -55,7 +55,7 @@ function NavBar() {
                     </li>
                     <li className='ml-8'>
                         <a href='mailto:ahmed1@usf.edu' target='_blank' rel='noopener noreferrer'>
-                            <button className='font-mono relative cursor-pointer before:content-[""] 
+                            <button className='font-mono font-medium relative cursor-pointer before:content-[""] 
                             before:absolute before:w-[130%] before:h-1 before:bg-black before:bottom-[-8px] before:left-[-15%] before:origin-left 
                             before:scale-x-0 before:transition-transform before:duration-300 hover:before:scale-x-100'>
                                 <span className='relative z-10'>//CONTACT</span>
@@ -64,36 +64,34 @@ function NavBar() {
                     </li>
                 </ul>
             </div>
-            {/* overlay for blur effect */}
-            <div className={`fixed inset-0 ${isMobileMenuOpen ? 'backdrop-blur-sm' : ''} z-40 pointer-events-none`}></div>
             {/* mobile menu */}
-            <div className={`fixed top-0 right-0 h-full w-3/4 bg-gray-100 p-8 transition-transform duration-500 transform ${isMobileMenuOpen ? 'translate-x-0' : 'translate-x-full'} md:hidden z-50`}>
+            <div className={`fixed top-0 right-0 h-full w-full bg-gray-100 p-8 transition-transform duration-500 transform ${isMobileMenuOpen ? 'translate-x-0' : 'translate-x-full'} md:hidden z-50`}>
                 <button onClick={toggleMobileMenu} className="text-3xl absolute top-6 right-4">
-                    <FaAnglesRight />
+                    <RiCloseLine />
                 </button>
-                <ul className='flex flex-col items-start mt-8'>
-                    <li className='mb-4'>
-                        <Link to='about' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono text-xl' onClick={toggleMobileMenu}>
+                <ul className='flex flex-col items-start mt-12'>
+                    <li className='mb-6'>
+                        <Link to='about' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono font-medium text-xl' onClick={toggleMobileMenu}>
                             // ABOUT
                         </Link>
                     </li>
-                    <li className='mb-4'>
-                        <Link to='research' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono text-xl' onClick={toggleMobileMenu}>
+                    <li className='mb-6'>
+                        <Link to='research' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono font-medium text-xl' onClick={toggleMobileMenu}>
                             // RESEARCH
                         </Link>
                     </li>
-                    <li className='mb-4'>
-                        <Link to='experience' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono text-xl' onClick={toggleMobileMenu}>
+                    <li className='mb-6'>
+                        <Link to='experience' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono font-medium text-xl' onClick={toggleMobileMenu}>
                             // EXPERIENCE
                         </Link>
                     </li>
-                    <li className='mb-4'>
-                        <Link to='projects' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono text-xl' onClick={toggleMobileMenu}>
+                    <li className='mb-6'>
+                        <Link to='projects' smooth={true} duration={500} hashSpy={true} offset={-navBarHeight} className='font-mono font-medium text-xl' onClick={toggleMobileMenu}>
                             // PROJECTS
                         </Link>
                     </li>
-                    <li className='mb-4'>
-                        <a href='mailto:ahmed1@usf.edu' target='_blank' rel='noopener noreferrer' className='font-mono text-xl' onClick={toggleMobileMenu}>
+                    <li className='mb-6'>
+                        <a href='mailto:ahmed1@usf.edu' target='_blank' rel='noopener noreferrer' className='font-mono font-medium text-xl' onClick={toggleMobileMenu}>
                             // CONTACT
                         </a>
                     </li>
