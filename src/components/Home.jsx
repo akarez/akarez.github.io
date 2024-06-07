a08fde215b0f5bddbacd25ef7ecbb5c5be854c83
Samir Ahmed <samirakarez@gmail.com>
2024-06-07 15:16:42 -0400

    update mobile styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import Profile from '../assets/samirahmed-pfp.png';
 import Resume from '../assets/samirahmed-resume.pdf';
-import { FaFileLines, FaLinkedin, FaGithub, FaGoogleScholar, FaFlaskVial } from 'react-icons/fa6';
+import { FaFileLines, FaLinkedin, FaGithub, FaGoogleScholar, FaFlask } from 'react-icons/fa6';
 
 function Home() {
     return (
-        <div id='home' className='flex justify-center items-center h-[85vh] bg-gray-100'>
-            <div className='flex w-full max-w-6xl' style={{ transform: 'translateX(-100px)' }}>
-                <div className='flex-shrink-0 w-1/2 flex justify-end items-center'>
-                    <img src={Profile} alt='Samir Ahmed' className='max-w-xs max-h-xs object-cover rounded-md' />
+        <div id='home' className='flex flex-col justify-center items-center md:h-[80vh] h-[70vh] bg-gray-100 py-10'>
+            <div className='flex flex-col md:flex-row w-full max-w-6xl md:transform md:-translate-x-24'>
+                <div className='flex justify-center md:justify-end w-full md:w-1/2 mb-8 md:mb-0 px-16'>
+                    <img src={Profile} alt='' className='object-cover rounded-md hidden md:block' />
                 </div>
-                <div className='flex flex-col justify-center items-start pl-8'>
-                    <h1 className='text-lg text-gray-800'>Hi there! I am</h1>
-                    <h2 className='text-6xl font-medium'>Samir Ahmed.</h2>
-                    <p className='text-lg mt-4'>
-                        I am a Computer Science and Engineering graduate student at the University of South Florida,
-                        and Research Assistant at the USF Interface Research Lab.
+                <div className='flex flex-col justify-center items-start w-full md:w-1/2 pl-8'>
+                    <h1 className='text-base md:text-lg text-gray-800 mb-1'>Hi there! I am</h1>
+                    <h2 className='text-5xl font-medium' style={{ marginLeft: '-4px' }}>
+                        Samir Ahmed.
+                    </h2>
+                    <p className='text-base md:text-lg mt-4'>
+                        I am a Computer Science and Engineering Ph.D. student at the University of South Florida, and Research Assistant at the USF Interface Research Lab. 
                     </p>
                     <div className='flex mt-5 space-x-5'>
                         <a href={Resume} target='_blank' rel='noopener noreferrer' className='transition-transform duration-300 hover:scale-110'>
@@ -31,7 +32,7 @@ function Home() {
                             <FaGoogleScholar size={26} className='text-black' />
                         </a>
                         <a href='https://cse.usf.edu/~rkaram/researchfocus.html' target='_blank' rel='noopener noreferrer' className='transition-transform duration-300 hover:scale-110'>
-                            <FaFlaskVial size={26} className='text-black' />
+                            <FaFlask size={26} className='text-black' />
                         </a>
                     </div>
                 </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,44 +1,65 @@
-import React from 'react'
-import Cyclone from '../assets/projects/Cyclone.png'
-import Bumblebee from '../assets/projects/Bumblebee.png'
+import React from 'react';
+import { FaArrowUpRightFromSquare } from 'react-icons/fa6';
+import Cyclone from '../assets/projects/cyclone.png';
+import Bumblebee from '../assets/projects/bumblebee.png';
+import CycloneSide from '../assets/projects/cyclone-side.png';
+import BumblebeeSide from '../assets/projects/bumblebee-side.png';
+import { useMediaQuery } from 'react-responsive';
 
 const projects = [
     {
         id: 1,
         title: 'CYCLONE',
-        description: `Temperature-controlled brushless fan driver for small space cooling applications.
-                      The fan speed is controlled with a PWM signal to a MOSFET gate, and it is linerarly 
-                      proportional to the temperature measured by the integrated MCP9808 I2C temperature sensor.`,
+        description: `The CYCLONE is a temperature controlled brushless fan driver for small space cooling applications.
+                      The fan speed is controlled via PWM with an STM32F030 microcontroller and MCP9808 I2C temperature sensor.`,
         image: Cyclone,
+        imageSide: CycloneSide,
+        link: 'https://github.com/akarez/cyclone',
     },
     {
         id: 2,
         title: 'BUMBLEBEE',
-        description: `Wireless 6-key mechanical keyboard designed as a drop-in replacement for the RAMA M6. The bumblebee features an nRF52840, per-key RGB, battery charging and reading,
-        and hot-swap MX switch sockets.`,
+        description: `The bumblebee is a wireless mechanical keyboard designed as a drop-in replacement for the RAMA M6. 
+                      It features an nRF52840 microcontroller, addressable RGB, battery charging circuit, and hot-swap switch sockets.
+                      The keymap can be configured with ZMK.`,
         image: Bumblebee,
+        imageSide: BumblebeeSide,
+        link: 'https://github.com/akarez/bumblebee',
     },
 ];
 
 function Projects() {
+    const isDesktopOrLaptop = useMediaQuery({
+        query: '(min-width: 768px)',
+    });
+
     return (
-        <div id='projects' className='bg-gray-100 py-20'>
-            <div className='max-w-4xl mx-auto px-6'>
-                <h1 className='text-4xl font-medium mb-8 font-mono'>RECENT PROJECTS</h1>
+        <div id='projects' className='bg-gray-100 py-20'> 
+            <div className='flex flex-col max-w-4xl mx-auto px-6'>
+                <h1 className='mb-16 text-2xl text-center font-medium md:text-3xl'>RECENT PROJECTS</h1>
                 {projects.map((project, index) => (
                     <div key={project.id} className={`flex flex-col md:flex-row ${index % 2 === 0 ? 'md:flex-row-reverse' : ''} mb-12`}>
-                        <div className='w-full md:w-1/2 flex justify-center items-center mb-16 md:mb-0'>
-                            <img src={project.image} alt={project.title} className='max-w-xs max-h-xs object-contain rounded-md' />
+                        <div className='w-full md:w-1/2 flex justify-center items-center mb-6'>
+                            <img 
+                                src={isDesktopOrLaptop ? project.imageSide : project.image} 
+                                alt={project.title} 
+                                className='max-w-sm max-h-sm md:max-w-xs md:max-h-xs object-contain rounded-md' 
+                            />
                         </div>
-                        <div className='w-full md:w-1/2 flex flex-col justify-center items-start pl-8 md:pl-9'>
-                            <h2 className='text-xl font-medium mb-4 font-mono'>{project.title}</h2>
+                        <div className={`w-full md:w-1/2 flex flex-col justify-center items-start pl-2 md:pl-2 mb-6 ${index % 2 === 0 ? 'md:items-end md:text-right' : ''}`}>
+                            <h2 className='text-xl font-medium mb-2 font-mono flex items-center'>
+                                {project.title}
+                                <a href={project.link} target='_blank' rel='noopener noreferrer' className='ml-2 transition-transform duration-300 hover:scale-110'>
+                                    <FaArrowUpRightFromSquare size={16} className='text-black' />
+                                </a>
+                            </h2>
                             <p className=''>{project.description}</p>
                         </div>
                     </div>
                 ))}
             </div>
         </div>
-    )
+    );
 }
 
 export default Projects;
